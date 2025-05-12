441a968e754f85fbedb675b0abd31491ccbb5948
Julie Biaunie <julie@Julies-MacBook-Pro.local>
2025-05-12 15:07:07 -0400

    nav and footer added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,20 +2,59 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to Laravel Starter</title>
+    <title>Weather Dashboard</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <h1>Weather Dashboard</h1>
-    <p>This is a starting point for your assignment.</p>
+    <nav class="bg-blue-300 flex justify-center flex-wrap h-16 gap-6 text-black font-medium">
+        <a class="hover:text-white" href="/">Home</a>
+        <a class="hover:text-white" href="https://google.com" target="_blank" rel="noopener noreferrer">Google</a>
+    </nav>
     <div>
-        <p>Current Temperature: <span>{{$current->temperature_2m}}</span></p>
-        <p>Current Condition: Sunny</p>
-    </div>
-    <div>
-        <h2>Additional Details</h2>
-        <p>Humidity: <span>{{$current->relative_humidity_2m}}</span></p>
-        <p>Wind Speed: <span>{{$current->wind_speed_10m}}</span></p>
+      <ul class="divide-y divide-gray-200">
+        <li class="flex gap-6 p-4">
+          <span class="font-medium text-gray-800">Current Temperature:</span>
+          <span class="text-gray-600">{{$current->temperature_2m}}°C</span>
+        </li>
+        <li class="flex gap-6 p-4">
+          <span class="font-medium text-gray-800">Current Condition</span>
+          <span class="text-gray-600">☀️</span>
+        </li>
+        <li class="flex gap-6 p-4">
+          <span class="font-medium text-gray-800">Humidity:</span>
+          <span class="text-gray-600">{{$current->relative_humidity_2m}}</span>
+        </li>
+        <li class="flex gap-6 p-4">
+          <span class="font-medium text-gray-800">Wind Speed:</span>
+          <span class="text-gray-600">{{$current->wind_speed_10m}}km/h</span>
+        </li>
+      </ul>
     </div>
 </body>
+<footer class="flex flex-col space-y-10 justify-center m-10 bg-blue-300">
+
+    <nav class="flex justify-center flex-wrap gap-6 text-black font-medium">
+        <a class="hover:text-white" href="/">Home</a>
+        <a class="hover:text-white" href="https://google.com">Google</a>
+    </nav>
+
+    <div class="flex justify-center space-x-5">
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
+        </a>
+        <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
+        </a>
+    </div>
+    <p class="text-center text-black font-medium">&copy; 2025 Weather Dashboard</p>
+</footer>
 </html>
